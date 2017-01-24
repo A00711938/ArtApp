db458babf57a6cc22c4f26699156fe6c06fd3597
Hiram Cabrera <takitocabrera@gmail.com>
2017-01-24 14:03:35 -0800

    Testing Github Changes

--- app/src/main/java/a3t/groupartapp/comp3717/artapp/MainActivity.java
+++ app/src/main/java/a3t/groupartapp/comp3717/artapp/MainActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private int one = 1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
