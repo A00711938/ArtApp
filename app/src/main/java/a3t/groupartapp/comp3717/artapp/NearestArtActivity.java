63f231512953402835f807c47e194306bae941e0
Hiram Cabrera <takitocabrera@gmail.com>
2017-01-24 22:53:46 -0800

    Finished Nearest Art and Welcome Activity Screen Templates

---
+++ app/src/main/java/a3t/groupartapp/comp3717/artapp/NearestArtActivity.java
@@ -0,0 +1,13 @@
+package a3t.groupartapp.comp3717.artapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NearestArtActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nearest_art);
+    }
+}
