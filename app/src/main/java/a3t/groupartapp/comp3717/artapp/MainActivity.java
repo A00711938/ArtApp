77edc1c233a5b7dd4787a6d518eac2bcfa25ad85
Hiram Cabrera <takitocabrera@gmail.com>
2017-01-25 22:48:14 -0800

    Fixed the buttons from some of the screens. Waiting for Hugh to upload his files.

--- app/src/main/java/a3t/groupartapp/comp3717/artapp/MainActivity.java
+++ app/src/main/java/a3t/groupartapp/comp3717/artapp/MainActivity.java
@@ -1,7 +1,9 @@
 package a3t.groupartapp.comp3717.artapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +13,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void startApp(View view){
+        Intent i = new Intent(this, WelcomeScreenActivity.class);
+        startActivity(i);
     }
 }
--- app/src/main/java/a3t/groupartapp/comp3717/artapp/WelcomeScreenActivity.java
+++ app/src/main/java/a3t/groupartapp/comp3717/artapp/WelcomeScreenActivity.java
@@ -1,7 +1,11 @@
 package a3t.groupartapp.comp3717.artapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import static android.os.Build.VERSION_CODES.M;
 
 public class WelcomeScreenActivity extends AppCompatActivity {
 
@@ -9,5 +13,14 @@ public class WelcomeScreenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome_screen);
+    }
+
+    public void goToNearestArt(View view) {
+        Intent i = new Intent(this, NearestArtActivity.class);
+        startActivity(i);
+    }
+
+    public void goToMyCollection(View view){
+
     }
 }
