74c180f7f01415546c9835099a18ccc05c21d748
HughSong <forever.love.hugh@gmail.com>
2017-02-16 10:06:06 -0800

    The display of detail page

--- app/src/main/java/a3t/groupartapp/comp3717/artapp/ArtDetailsActivity.java
+++ app/src/main/java/a3t/groupartapp/comp3717/artapp/ArtDetailsActivity.java
@@ -1,16 +1,47 @@
 package a3t.groupartapp.comp3717.artapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.List;
 
 public class ArtDetailsActivity extends AppCompatActivity {
 
     @Override
+
+
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_art_details);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        String name = null;
+        List<String> comment = null;
+        List<String> img;
+        Intent intent = getIntent();
+        String positionName = intent.getStringExtra("position");
+        DataHelper helper = new DataHelper(getApplicationContext());
+        helper.init();
+        List<Art> arts = helper.getArts();
+        for(Art art : arts) {
+            name = art.getName();
+            if(name.equalsIgnoreCase(positionName)) {
+                comment = art.getComment();
+                img =  art.getImages();
+                break;
+            }
+        }
+
+        EditText commentField = (EditText) findViewById(R.id.comment);
+        TextView nameField = (TextView) findViewById(R.id.name);
+            commentField.setText(comment.get(0));
+            nameField.setText(name);
     }
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
--- app/src/main/java/a3t/groupartapp/comp3717/artapp/CollectionPageActivity.java
+++ app/src/main/java/a3t/groupartapp/comp3717/artapp/CollectionPageActivity.java
@@ -5,6 +5,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class CollectionPageActivity extends AppCompatActivity {
 
@@ -16,12 +18,26 @@ public class CollectionPageActivity extends AppCompatActivity {
     }
 
     public void goToArtDetails(View view){
+        String name = "Alphabet Ball";
+
         Intent i = new Intent(this, ArtDetailsActivity.class);
+        i.putExtra("position",name);
         startActivity(i);
     }
 
+    public void goToArtDetails1(View view){
+
+        String name ="Kiwanis Mosaic";
+        Intent i = new Intent(this, ArtDetailsActivity.class);
+        i.putExtra("position",name);
+        startActivity(i);
+    }
+
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
+
+
         if (item.getItemId() == android.R.id.home ) {
             finish();
             return true;
